65ecfe735c91c76ceab71318fde438147307e052
itxmeh <nshumeigi@gmail.com>
2024-07-03 19:14:56 +0200

    Change Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:note_app/firebase_options.dart';
+import 'package:note_app/screens/home_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-      const MyApp());
+     const NoteApp());
 }
-class  extends StatelessWidget {
-  const ({super.key});
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
     );
   }
 }
+
+
